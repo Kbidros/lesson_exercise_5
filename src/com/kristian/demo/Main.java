c2eafa5ca63eee3befaff4eeefc2359512985696
Kbidros <kristianbidros@gmail.com>
2023-09-23 20:16:49 +0200

    uppgift 2

--- src/com/kristian/demo/Main.java
+++ src/com/kristian/demo/Main.java
@@ -6,10 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        //Uppgift 1
         Scanner sc = new Scanner(System.in);
         System.out.println("What's your name player #1?");
         String playerOne = sc.nextLine();
         System.out.println("Player #1: " + playerOne);
+
+
+        //Uppgift 2
+        System.out.println("Enter a number");
+        int x = sc.nextInt();
+
+        for (int i = 0; i < x; i++) {
+
+            System.out.println("Loop #" + (i + 1));
+        }
+
 
 
 
