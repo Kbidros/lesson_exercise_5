559795b1f446f86b31308b2c7b1f90eb812831e3
Kbidros <kristianbidros@gmail.com>
2023-09-23 20:04:36 +0200

    uppgift 1

---
+++ src/com/kristian/demo/Main.java
@@ -0,0 +1,21 @@
+package com.kristian.demo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What's your name player #1?");
+        String playerOne = sc.nextLine();
+        System.out.println("Player #1: " + playerOne);
+
+
+
+
+        }
+
+
+
+    }
